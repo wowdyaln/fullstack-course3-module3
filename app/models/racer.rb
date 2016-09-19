f0929b785b31c19ad2a91e3984f4611c7fef3f22
wowdyaln <rogersmk@gmail.com.tw>
2016-09-20 01:49:03 +0800

    field 增改別名；Racer model 存到 racer1 collection

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,3 +1,10 @@
 class Racer
   include Mongoid::Document
+
+  store_in collection: "racer1"
+
+  field :dob, as: :date_of_birth, type: Date
+  field :gender, type: String
+  field :fn, as: :first_name, type: String
+  field :ln, as: :last_name, type: String
 end
