b17a402d746807a9c34aeba69cc72779a2a511c0
wowdyaln <rogersmk@gmail.com.tw>
2016-09-20 17:49:07 +0800

    Racer embeds_one Address

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -3,4 +3,8 @@ class Racer
   field :fn, as: :first_name, type: String
   field :ln, as: :last_name, type: String
   field :dob, as: :date_of_birth, type: Date
+
+  embeds_one :primary_address, class_name: "Address"
+
+
 end
