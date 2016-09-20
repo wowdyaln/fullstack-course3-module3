05fa4fdb4f88e080376671944bdf4d1262777f2f
wowdyaln <rogersmk@gmail.com.tw>
2016-09-20 14:49:59 +0800

    初始設定

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'mongoid', '~> 5.0.0'
 # Use Unicorn as the app server
 # gem 'unicorn'
-
+gem 'awesome_rails_console'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Contests
   class Application < Rails::Application
+    # Boostraps mongoid within applications -- like rails console
+    Mongoid.load!('./config/mongoid.yml')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
