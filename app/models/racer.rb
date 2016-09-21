637eea522c73ce7412d25786b4c901aca04811fc
wowdyaln <rogersmk@gmail.com.tw>
2016-09-21 17:44:08 +0800

    很複雜的在Racer 增加 'races' 實列方法

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -5,6 +5,13 @@ class Racer
   field :dob, as: :date_of_birth, type: Date
 
   embeds_one :primary_address, class_name: "Address", as: :addressable
+
   # has_many :races, class_name: "Entrant"
+    def races
+      Contest.where(:"entrants.racer_id"=>self.id).map do |contest|
+        contest.entrants.where(:racer_id=>self.id).first
+      end
+    end
+
 
 end
